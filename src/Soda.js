a9e0ef2d9e017f7f7e4bb7580573bb8f02345eac
Jeremy Sobczak <jeremy@jeremys-mbp.lan>
2023-06-21 16:01:24 -0400

    Initial commit. Framework only, no functionality.

---
+++ src/Soda.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// A type of snack component. From each snack component, you should be able to go back to the main VendingMachine component.
+
+function Soda() {
+
+}
+
+export default Soda
---
+++ src/VendingMachine.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Candybar from "./Candybar";
+import Chips from "./Chips";
+import Soda from "./Soda";
+
+// Shows a page with a list of snacks that you can get from the vending machine.
+// By clicking on an item in the VendingMachine you can view one of the snacks inside of the machine. Clicking should update the URL and show the snack that you’ve clicked on.
+
+function VendingMachine() {
+
+}
+
+export default VendingMachine
